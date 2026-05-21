de957330dd4e7c1318231d236daf842d0a4f11f7
jmot4 <jmota@gtech-services.com>
2026-05-21 15:49:13 -0600

    Se termina solucion.

--- MauiNavigation/Pages/DetallePage.xaml.cs
+++ MauiNavigation/Pages/DetallePage.xaml.cs
@@ -1,9 +1,28 @@
-namespace MauiNavigation.Pages;
+using MauiNavigation.Models;
 
+namespace MauiNavigation.Pages;
 public partial class DetallePage : ContentPage
 {
-	public DetallePage()
+	private readonly Producto _producto;
+	public DetallePage(Producto producto)
 	{
 		InitializeComponent();
+		_producto= producto;
+
+		lblNombre.Text=_producto.Nombre;
+		lblPrecio.Text = _producto.PrecioFormateado;		
+		lblDescripcion.Text= _producto.Descripcion;
+        lblCategoria.Text= _producto.Categoria;
+        lblId.Text = $"ID: {_producto.Id}";
+    }
+
+	private async void OnAgregarClicked(object sender, EventArgs e) 
+	{
+		await DisplayAlert("Agregado",$"{_producto.Nombre} fue agregado al carrito.","Ok");
+	}
+
+	private async void OnVolverClicked(object sender, EventArgs e) 
+	{
+		await Navigation.PopAsync();
 	}
 }
